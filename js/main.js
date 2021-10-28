a638e70a01a0e02cea7dcef7c10840ee73e95ffc
Jihad <jihad.marine@gmail.com>
2021-10-28 23:16:14 +0600

    doing responsive

---
+++ js/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $('.hambarger').click(function(){
+        $('.navbar1').fadeIn();
+    });
+    $('.cancel').click(function(){
+        $('.navbar1').fadeOut();
+    })
+})
